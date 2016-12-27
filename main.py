b263abee29b9ba02f2401ed3295455d48652b6a3
Jaime Munoz <jaimemunz@gmail.com>
2016-12-27 17:22:51 -0600

    Completed timer display and did refactoring

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from Tkinter import *
 from timer import Timer
+import time
 
 class App:
     
@@ -7,49 +8,76 @@ class App:
         frame = Frame(master)
         frame.pack()
         self.task_list = []
-        self.timer = Timer()
-        
+        self.timer_var = StringVar()
+        self.task_time = 25
+        self.timer_main = Timer(frame, self.timer_var)
 
         # Create text widget to display list
-        self.task_display = Text(frame, spacing1=1, height=10, width=70)
+        self.task_list_display(frame)
+        
+        # Entry widget
+        self.task_entry(frame)
+
+        # Add task button
+        self.add_task_button(frame)
+
+        # Quit button
+        self.quit_button(frame)
+
+        
+    def task_list_display(self, frame):
+        """
+        Create the label on which the tasks will be dislpayed
+        """
+        self.task_display = Text(
+            frame, spacing1=1, height=10, width=70
+        )
         self.task_display.grid(row=0, column=0, sticky=N)
         self.task_display.config(state=DISABLED)
 
-        # Entry widget
+    def task_entry(self, frame):
+        """
+        Creates entry widget to add taks to list
+        """
         self.e = Entry(frame, width=93)
-        self.e.grid(row =1, sticky=W)
-
-        # Add task button
-        self.b = Button(frame, text="Add Task", width=10, command=self.get_entry)
         self.e.bind("<Return>", self.get_entry)
-        self.b.grid(row=1, column=2, sticky=W)
+        self.e.grid(row =1, column=0, sticky=W)
 
-        # Timer text
-        self.timer_display = Text(frame, height=5,width=10)
-        self.timer_display.insert(END, self.timer.time)
-        self.timer_display.grid(row=0, column=1)
-
-        
-        # Quit button
-        self.quit_button = Button(
-            frame, text = "QUIT", fg="red", command=frame.quit
-            )
-        self.quit_button.grid(row=2, padx=5, sticky=S)
-
+    def add_task_button(self, frame):
+        """
+        Creates button to add the task in the entry box
+        to the list
+        """
+        self.b = Button(
+            frame, text="Add Task", width=16,
+            command=self.get_entry
+        )
+        self.b.grid(row=1, column=1, sticky=W)
+    
     def get_entry(self, event=None):
-        # Gets task from entry widget
+        """ Gets task from entry widget """
         self.task_list.append(self.e.get())
         self.e.delete(0,END)
         self.display_text()
-
+        
     def display_text(self):
-        # Make the text widget editable
+        """ Make the text widget editable """
         self.task_display.config(state=NORMAL)
         self.task_display.delete(1.0, END)
         for task in self.task_list:
             self.task_display.insert(END, task+ "\n")
         self.task_display.config(state=DISABLED)
 
+    def quit_button(self, frame):
+        """ 
+        Creates teh quit button
+        """
+        self.quit_button = Button(
+            frame, text = "QUIT", fg="red",
+            command=frame.quit
+            )
+        self.quit_button.grid(row=3, column=1, padx=5)    
+   
 
 root = Tk()
 app = App(root)
